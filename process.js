1323658981fc358fb8c90f7ccd77a337f352f6a4
Romeo110 <99770762+Romeo110@users.noreply.github.com>
2024-04-26 00:35:51 -0400

    Add files via upload

--- process.js
+++ process.js
@@ -1,32 +1,35 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
+const path = require('path'); // Import the path module
 
 const app = express();
+const port = process.env.PORT || 3000;
 const url = 'mongodb+srv://romeo:romello1@mycluster.ficth39.mongodb.net/?retryWrites=true&w=majority&appName=mycluster';
 const dbName = 'Stock';
 const collectionName = 'PublicCompanies';
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname); // Set the views directory to the current directory
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
+// Home view route
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/process', (req, res) => {
+// Process view route
+app.get('/process', async (req, res) => {
     const searchTerm = req.query.searchTerm;
     const searchType = req.query.searchType;
 
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-        if (err) {
-            console.error('Error connecting to MongoDB:', err);
-            return;
-        }
+    try {
+        const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
 
+        // Construct query based on search type
         const query = {};
         if (searchType === 'ticker') {
             query.stockTicker = searchTerm;
@@ -34,22 +37,23 @@ app.get('/process', (req, res) => {
             query.companyName = searchTerm;
         }
 
-        collection.find(query).toArray((err, result) => {
-            if (err) {
-                console.error('Error executing query:', err);
-                client.close();
-                return;
-            }
+        // Find matching documents in the collection
+        const result = await collection.find(query).toArray();
 
-            console.log('Search Results:', result);
-            // Render the results.html template with the search results
-            res.render('results.html', { results: result });
-            client.close();
-        });
-    });
+        // Display search results in the console
+        console.log('Search Results:', result);
+
+        // Render the results.ejs view with search results
+        res.render(path.join(__dirname, 'results.ejs'), { results: result });
+
+        // Close MongoDB connection
+        client.close();
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+        res.status(500).send('Error connecting to database');
+    }
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });

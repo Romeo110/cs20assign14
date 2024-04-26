4ff0be8fb2c7d2a5bc9e28881e3a8670db445905
Romeo110 <99770762+Romeo110@users.noreply.github.com>
2024-04-26 13:03:52 -0400

    Add files via upload

--- process.js
+++ process.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const path = require('path'); // Import the path module
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const url = 'mongodb+srv://romeo:romello1@mycluster.ficth39.mongodb.net/?retryWrites=true&w=majority&appName=mycluster';
 const dbName = 'Stock';
 const collectionName = 'PublicCompanies';
-
-app.set('view engine', 'ejs');
-app.set('views', __dirname); // Set the views directory to the current directory
 
 app.use(express.static('public'));
 
@@ -43,11 +40,11 @@ app.get('/process', async (req, res) => {
         // Display search results in the console
         console.log('Search Results:', result);
 
-        // Render the results.ejs view with search results
-        res.render(path.join(__dirname, 'results.ejs'), { results: result });
-
         // Close MongoDB connection
         client.close();
+
+        // Send the search results as JSON
+        res.json(result);
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
         res.status(500).send('Error connecting to database');
